ef07acfccbfe49e0d5be41194d8bc84b459e6c5b
LevyAM <levyzudo@msn.com>
2021-12-22 11:26:12 -0300

    feat: subnets created

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,22 @@
 resource "aws_vpc" "new-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    "Name" = "terraform-vpc"
+    "Name" = "${var.prefix}-vpc"
   }
+}
+
+data "aws_availability_zones" "available" {}
+# output "az" {
+#   value = "${data.aws_availability_zones.available.names}"
+# }
+
+resource "aws_subnet" "subnets" {
+    count = 2
+    availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+    vpc_id = "${aws_vpc.new-vpc.id}"
+    cidr_block = "10.0.${count.index}.0/24"
+    map_public_ip_on_launch = true
+    tags = {
+        "name" = "${var.prefix}-subnet-${count.index}"
+    }
 }
